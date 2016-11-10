681ecd02b8d3218330af391545bb125dd9b1e37b
Daniel Mercier <mercierdaniel91@gmail.com>
2016-11-10 18:22:18 +0100

    Use of inductive for accepts_nae

--- Regex2nfa.v
+++ Regex2nfa.v
@@ -14,6 +14,18 @@ Definition word := list A.
 
 Definition nae := nfa (option A) S.
 
+Definition eps (nae : nae) := step nae None.
+
+(*On utilise steps car mieux pour raisonner après, blocage avec l'autre version de accepts_eps_from*)
+Inductive steps_nae nae : list A -> Ensemble (S * S) :=
+  | In_step_nil : forall p q, eps nae (p, q) -> steps_nae nae [] (p, q)
+  | In_step_cons :
+      forall h q sa sb sc sd,
+        eps nae (sa, sb) ->
+        step nae (Some h) (sb, sc) ->
+        steps_nae nae q (sc, sd) ->
+          steps_nae nae (h::q) (sa, sd).
+
 Inductive accepts_eps_from (nae: nae) (s: S) : list A -> Prop :=
   | acc_nil : In _ (fin nae) s -> accepts_eps_from nae s []
   | acc_cons : forall e h q, In _ (next nae (Some h) s) e -> accepts_eps_from nae e q -> accepts_eps_from nae s (h::q)
@@ -40,13 +52,22 @@ Definition atom (a: A): bitsNAe :=
     (atom_next a)
     (Singleton _ [false]).
 
+Inductive prepend_list_set (A: Type) (b: A) (ens: Ensemble (list A)) : Ensemble (list A) :=
+  | in_prep : forall q, In _ ens q -> prepend_list_set b ens (b::q).
+
+(*Inductive alt_next (nae1 nae2: bitsNAe): option A -> list bool -> Ensemble (list bool) :=
+  | nextL : forall a q s, In _ (next nae1 a s) q -> alt_next nae1 nae2 a (true::q) s
+  | nextR : forall a q s, In _ (next nae2 a s) q -> alt_next nae1 nae2 a (false::q) s.*)
+
 Inductive alt_next (nae1 nae2: bitsNAe): option A -> list bool -> Ensemble (list bool) :=
+  | nextNL : alt_next nae1 nae2 None [] (true::start nae1)
+  | nextNR : alt_next nae1 nae2 None [] (false::start nae2)
   | nextL : forall a q s, In _ (next nae1 a s) q -> alt_next nae1 nae2 a (true::q) s
   | nextR : forall a q s, In _ (next nae2 a s) q -> alt_next nae1 nae2 a (false::q) s.
 
 Inductive alt_fin (nae1 nae2: bitsNAe): Ensemble (list bool) :=
-  | finL : forall s, In _ (fin nae1 s) -> alt_fin nae1 nae2 (true::s)
-  | finR : forall s, In _ (fin nae1 s) -> alt_fin nae1 nae2 (false::s)
+  | finL : forall s, In _ (fin nae1) s -> alt_fin nae1 nae2 (true::s)
+  | finR : forall s, In _ (fin nae1) s -> alt_fin nae1 nae2 (false::s).
 
 Definition alt (nae1: bitsNAe) (nae2: bitsNAe): bitsNAe :=
   let start1 := start nae1 in
@@ -61,14 +82,9 @@ Definition alt (nae1: bitsNAe) (nae2: bitsNAe): bitsNAe :=
   mkNfa
     ([])
     (alt_next nae1 nae2)
-    (fun r =>
-      match r with
-      | [] => False
-      | true::q => fin1 q 
-      | false::q => fin2 q
-      end).
+    (alt_fin nae1 nae2).
 
-Definition conc (nae1: bitsNAe) (nae2: bitsNAe): bitsNAe :=
+(*Definition conc (nae1: bitsNAe) (nae2: bitsNAe): bitsNAe :=
   let start1 := start nae1 in
   let start2 := start nae2 in
 
@@ -104,7 +120,7 @@ Section Test.
 Definition rexp1 := Atom 1.
 Definition nfae1 := atom 1.
 
-Goal accepts_eps nfae1 [1].
+Lemma acc_nfae1 : accepts_eps nfae1 [1].
   apply acc_cons with (e := [false]).
   compute.
   apply nextA.
@@ -112,6 +128,38 @@ Goal accepts_eps nfae1 [1].
   compute.
   apply In_singleton.
 Qed.
+
+Definition nfae2 := alt (atom 1) (atom 2).
+
+Variable A: Type.
+
+(*Blocage....*)
+Goal forall (w : list A) (nae1 nae2 : bitsNAe A),
+        accepts_eps nae1 w -> accepts_eps nae2 w -> accepts_eps (alt nae1 nae2) w.
+
+  unfold accepts_eps.
+  intros w nae1 nae2 H H'.
+  simpl.
+  apply acc_none with (e := (false::(start nae2))).
+  *
+    unfold In.
+    simpl.
+    apply nextNR.
+  *
+  Print accepts_eps_from.
+  induction w.
+  
+
+
+Goal accepts_eps nfae2 [1].
+  Print acc_cons.
+  apply acc_none with (e := [true; true]).
+  unfold In.
+  simpl.
+  Print nextNL.
+  apply nextNL.
+  Print accepts_eps_from.
+  apply acc_cons with (e := .
 
 Goal forall x : nat, accepts (to_nfae (Atom x)) [Some x].
   intro x.
